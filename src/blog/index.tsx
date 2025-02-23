39f028e9acf6e87e0bc0e9d460b74591ed5fc38e
Francisco Hodge <hello@franciscohodge.com>
2025-02-23 15:45:21 -0500

    Pagination

--- src/blog/index.tsx
+++ src/blog/index.tsx
@@ -21,15 +21,17 @@ const Blog = data => {
     heroTitle,
     heroMessage,
     heroImageUrl,
-    sidebarAsideHtml
-  } = PRSS.getProp('vars') as IVars;
+    sidebarAsideHtml,
+    blogPosts,
+    currentPage,
+    totalPages
+  } = PRSS.getProp('vars') as any;
 
-  const links = PRSS.getJsonProp('vars.links') as ILink[];
-
+  const { rootPath } = data;
   const { content, title } = PRSS.getProp('item');
   const sidebarHtml = PRSS.getProp('sidebarHtml');
-
-  const items = PRSS.getItems('post', true);
+  const items = PRSS.getItems('post', true, blogPosts);
+  const adjustedRootPath = currentPage === 1 ? rootPath : `../${rootPath}`;
 
   return (
     <Page className="page-blog">
@@ -116,6 +118,48 @@ const Blog = data => {
                 <Aside name="sidebarAsideHtml" />
               </div>
             )}
+          </div>
+          <div className="row">
+            <div className="col">
+              <nav aria-label="Page navigation">
+                <ul className="pagination flex justify-content-center mt-2 mb-5">
+                  {currentPage > 1 && (
+                    <li className="page-item">
+                      <a
+                        className="page-link"
+                        href={`${adjustedRootPath}blog/${currentPage - 1 === 1 ? "" : currentPage - 1}`}
+                      >
+                        Previous
+                      </a>
+                    </li>
+                  )}
+                  {Array.from({ length: totalPages }).map((_, i) => {
+                    const pageNumber = i + 1;
+                    const isActive = currentPage === pageNumber;
+                    return (
+                      <li key={i} className="page-item">
+                        <a
+                          href={`${pageNumber === 1 ? `${adjustedRootPath}blog/` : `${adjustedRootPath}blog/${pageNumber}/`}`}
+                          className={cx('page-link', { active: isActive })}
+                        >
+                          {pageNumber}
+                        </a>
+                      </li>
+                    );
+                  })}
+                  {currentPage < totalPages && (
+                    <li className="page-item">
+                      <a
+                        className="page-link"
+                        href={`${adjustedRootPath}blog/${currentPage + 1}`}
+                      >
+                        Next
+                      </a>
+                    </li>
+                  )}
+                </ul>
+              </nav>
+            </div>
           </div>
         </div>
       </main>
